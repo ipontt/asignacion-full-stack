24b0f1590356468fa5c67753bddb08a4fba2cb88
Inti Pontt <inti.pontt@gmail.com>
2021-09-25 03:04:26 -0300

    Add placeholder layout.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Placeholder from './Placeholder.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-2xl mx-auto">
+        <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+          <div className="px-4 py-5 sm:px-6">
+            <Placeholder class="border-2 border-dashed border-gray-300 rounded bg-white w-full text-gray-200 h-8"/>
+          </div>
+          <div className="px-4 py-5 sm:p-6 grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <div class="lg:col-span-1">
+              <Placeholder class="border-2 border-dashed border-gray-300 rounded bg-white w-full text-gray-200 h-72"/>
+            </div>
+            <div class="lg:col-span-2">
+              <Placeholder class="border-2 border-dashed border-gray-300 rounded bg-white w-full text-gray-200 h-72"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
