6ecc271597f31496dbb4fe0f077e64d686b438e9
Inti Pontt <inti.pontt@gmail.com>
2021-09-27 14:31:13 -0300

    Add dynamic dot chart using tailwind variables for the color.

--- client/src/components/Comunas/Grafico.js
+++ client/src/components/Comunas/Grafico.js
@@ -1,8 +1,56 @@
-import Placeholder from '../Placeholder';
+import { Line } from 'react-chartjs-2';
+import { useEffect, useState } from 'react';
+import resolveConfig from 'tailwindcss/resolveConfig';
 
-function Grafico() {
+const tailwind = resolveConfig({});
+
+function Grafico({ casos }) {
+	const [chart, setChart] = useState('');
+
+	useEffect(() => {
+		if (casos.length !== 0) {
+			const labels = casos.map(caso => caso.fecha);
+			let dataset_1 = [];
+			casos.map(caso => caso.cantidad).forEach((cantidad, index) => {
+				if (cantidad !== null) {
+					dataset_1[index] = cantidad;
+				}
+			})
+			const data = {
+				labels: labels,
+				datasets: [
+					{
+						label: 'casos confirmados',
+						data: dataset_1,
+						fill: false,
+						backgroundColor: tailwind.theme.colors.red['500'],
+					},
+				],
+			};
+			const options = {
+				scales: {
+					yAxes: [
+						{
+							ticks: {
+								beginAtZero: false,tickMarkLength: 5,
+							},
+						},
+					],
+				},
+				height: '4rem',
+			};
+			setChart(<Line data={data} options={options} />);
+		}
+	}, [casos])
+
 	return (
-		<Placeholder className="w-full"/>
+		<div className="w-full flex flex-col">
+			<div className="py-2 align-middle inline-block min-w-full">
+				<div className="shadow border-2 border-gray-700 sm:rounded-tl-lg">
+					{chart}
+				</div>
+			</div>
+		</div>
 	);
 }
 
